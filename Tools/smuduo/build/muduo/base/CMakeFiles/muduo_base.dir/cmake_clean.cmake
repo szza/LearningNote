0ef01686bad394576020e28687af625907bf2fd5
szza <39589766+szza@users.noreply.github.com>
2019-12-14 22:39:23 +0800

    cmake source code

---
+++ Tools/smuduo/build/muduo/base/CMakeFiles/muduo_base.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/muduo_base.dir/Exception.cc.o"
+  "../../lib/libmuduo_base.pdb"
+  "../../lib/libmuduo_base.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/muduo_base.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
